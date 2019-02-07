31806f27b8b1ed190c452c2c191f9d2509667d69
yauheni_chyrkouski <evgen.box@live.ru>
2019-02-07 17:29:53 +0300

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FilterComponent } from './filter/filter.component';
+import { NewsListComponent } from './news-list/news-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilterComponent,
+    NewsListComponent
   ],
   imports: [
     BrowserModule,
